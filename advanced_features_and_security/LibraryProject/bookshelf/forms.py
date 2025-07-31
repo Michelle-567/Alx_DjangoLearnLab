9f3985b696be1c1e22bf8c97abe3ab01c08c3ff2
Michelle-567 <michellendirangu2001@gmail.com>
2025-07-31 16:55:10 +0300

    Added advanced features and security project with permissions and groups

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def clean_message(self):
+        data = self.cleaned_data['message']
+        # Additional custom validation if needed
+        return data
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -18,3 +18,18 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+from .forms import ExampleForm
+
+def form_example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Securely process form data
+            cleaned_data = form.cleaned_data
+            # ... logic
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
