d40da2d5e9aed0d18a21deb6e38f64ea8b0f5167
Michelle-567 <michellendirangu2001@gmail.com>
2025-07-31 18:48:55 +0300

    Add token authentication and permissions to API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet  # Make sure these are imported
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -17,4 +18,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),  # Automatically maps all CRUD routes
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
