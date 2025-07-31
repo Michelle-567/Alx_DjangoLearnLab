05410f301ffc7b2d746955cad3169f06fdae58f8
Michelle-567 <michellendirangu2001@gmail.com>
2025-07-31 15:20:21 +0300

    Add permission-protected book_list view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render
-from .models import Article
+# # Create your views here.
+# from django.contrib.auth.decorators import permission_required
+# from django.shortcuts import render
+# from .models import Article
 
-@permission_required('yourapp.can_view', raise_exception=True)
-def view_articles(request):
-    articles = Article.objects.all()
-    return render(request, 'articles.html', {'articles': articles})
+# @permission_required('yourapp.can_view', raise_exception=True)
+# def view_articles(request):
+#     articles = Article.objects.all()
+#     return render(request, 'articles.html', {'articles': articles})
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
